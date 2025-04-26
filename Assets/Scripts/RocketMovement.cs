5f727ad92f9cf43996dcd33dbd958c57ac6a19b6
MansurBlink <135321681+MansurBlink@users.noreply.github.com>
2025-04-27 00:18:20 +0530

    Player Setup
    Physics Based Movement
    Cinemachine Implemented.

--- Assets/Scripts/RocketMovement.cs
+++ Assets/Scripts/RocketMovement.cs
@@ -4,20 +4,56 @@ using UnityEngine.InputSystem;
 public class RocketMovement : MonoBehaviour
 {
     [SerializeField] InputAction thrust;
+    [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
+
+    [SerializeField] float rotationStrength = 100f;
     Rigidbody rb;
 
     private void OnEnable()
     {
         thrust.Enable();
+        rotation.Enable();
+
+    }
+
+    private void Start()
+    {
         rb = GetComponent<Rigidbody>();
     }
 
-    private void Update()
+    private void FixedUpdate()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    private void ProcessThrust()
     {
         if (thrust.IsPressed())
         {
-           rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
         }
+    }
+
+    private void ProcessRotation()
+    {
+        float rotationInput = rotation.ReadValue<float>();
+        if (rotationInput < 0)
+        {
+            ApplyRotation(rotationStrength);
+        }
+        else if (rotationInput > 0)
+        {
+            ApplyRotation(-rotationStrength);
+        }
+
+    }
+
+    private void ApplyRotation(float rotateThisFrame)
+    {
+        rb.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotateThisFrame * Time.fixedDeltaTime);
+        rb.freezeRotation = false;
     }
 }
