4dfe2f698651670a0ac936fb41a2b27b5d7769b8
MansurBlink <135321681+MansurBlink@users.noreply.github.com>
2025-04-27 22:08:36 +0530

    Particles and Cleaning Code
    Added Multiple Particles
    Make the code easier to read.

--- Assets/Scripts/RocketMovement.cs
+++ Assets/Scripts/RocketMovement.cs
@@ -7,8 +7,12 @@ public class RocketMovement : MonoBehaviour
     [SerializeField] InputAction rotation;
     [SerializeField] float thrustStrength = 100f;
     [SerializeField] float rotationStrength = 100f;
-    [SerializeField] AudioClip engineThrust;
+    [SerializeField] AudioClip engineThrustSFX;
+    [SerializeField] ParticleSystem mainEngineParticles;
+    [SerializeField] ParticleSystem leftThrustParticles;
+    [SerializeField] ParticleSystem rightThrustPartcles;
     Rigidbody rb;
+
 
 
     AudioSource audioSource;
@@ -37,17 +41,31 @@ public class RocketMovement : MonoBehaviour
     {
         if (thrust.IsPressed())
         {
-            rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
-            if (!audioSource.isPlaying)
-            {
-                audioSource.PlayOneShot(engineThrust);
-            }
+            StartThrusting();
         }
 
         else
         {
-            audioSource.Stop();
+            StopThrusting();
         }
+    }
+    private void StartThrusting()
+    {
+        rb.AddRelativeForce(Vector3.up * thrustStrength * Time.fixedDeltaTime);
+        if (!audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(engineThrustSFX);
+        }
+
+        if (!mainEngineParticles.isPlaying)
+        {
+            mainEngineParticles.Play();
+        }
+    }
+    private void StopThrusting()
+    {
+        audioSource.Stop();
+        mainEngineParticles.Stop();
     }
 
     private void ProcessRotation()
@@ -55,15 +73,42 @@ public class RocketMovement : MonoBehaviour
         float rotationInput = rotation.ReadValue<float>();
         if (rotationInput < 0)
         {
-            ApplyRotation(rotationStrength);
+            RotateRight();
         }
         else if (rotationInput > 0)
         {
-            ApplyRotation(-rotationStrength);
+            RotateLeft();
+        }
+        else
+        {
+            StopRotating();
         }
 
     }
+    private void RotateRight()
+    {
+        ApplyRotation(rotationStrength);
+        if (!leftThrustParticles.isPlaying)
+        {
+            leftThrustParticles.Stop();
+            rightThrustPartcles.Play();
+        }
+    }
+    private void RotateLeft()
+    {
+        ApplyRotation(-rotationStrength);
+        if (!rightThrustPartcles.isPlaying)
+        {
 
+            rightThrustPartcles.Stop();
+            leftThrustParticles.Play();
+        }
+    }
+    private void StopRotating()
+    {
+        leftThrustParticles.Stop();
+        rightThrustPartcles.Stop();
+    }
     private void ApplyRotation(float rotateThisFrame)
     {
         rb.freezeRotation = true;
